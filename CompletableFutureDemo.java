ddcbbac144064bc4a6cd963f9cc099aa0ce6dbb9
GitYue <260355617@qq.com>
2020-04-10 19:37:04 +0800

    Create CompletableFutureDemo.java

---
+++ CompletableFutureDemo.java
@@ -0,0 +1,31 @@
+
+import java.util.concurrent.CompletableFuture;
+
+public class CompletableFutureDemo {
+
+    public static void main(String[] args) throws Exception {
+        //同步，异步，异步回调
+
+        //同步
+                CompletableFuture<Void> completableFuture1 = CompletableFuture.runAsync(()->{
+                    System.out.println(Thread.currentThread().getName()+"\t completableFuture1");
+                });
+                completableFuture1.get();
+
+        //异步回调
+        CompletableFuture<Integer> completableFuture2 = CompletableFuture.supplyAsync(()->{
+            System.out.println(Thread.currentThread().getName()+"\t completableFuture2");
+            int i = 10/0;
+            return 1024;
+        });
+
+        completableFuture2.whenComplete((t,u)->{
+            System.out.println("-------t="+t);
+            System.out.println("-------u="+u);
+        }).exceptionally(f->{
+            System.out.println("-----exception:"+f.getMessage());
+            return 444;
+        }).get();
+
+    }
+}
