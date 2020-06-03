c6d3346f7219e0c541967322f63cb9658fdd8ba1
loulanyue <260355617@qq.com>
2020-06-03 21:50:01 +0800

    添加lock

---
+++ src/main/java/com/yfy/multithreading/demo/Lock8Demo04.java
@@ -0,0 +1,66 @@
+package com.yfy.multithreading.demo;
+
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * 8 lock
+ * 1 标准访问，请问先打印邮件还是短信
+ * 2 暂停4秒钟在邮件方法，请问先打印邮件还是短信
+ * 3 新增普通sayHello方法，请问先打印邮件还是hello
+ * 4 两部手机，请问先打印邮件还是短信
+ * 5 两个静态同步方法，同一部手机，请问先打印邮件还是短信
+ * 6 两个静态同步方法，2部手机，请问先打印邮件还是短信
+ * 7 1个静态同步方法，1个普通同步方法,同一部手机，请问先打印邮件还是短信
+ * 8 1个静态同步方法，1个普通同步方法,2部手机，请问先打印邮件还是短信
+ */
+public class Lock8Demo04 {
+    public static void main(String[] args) throws InterruptedException {
+        Phone phone = new Phone();
+        Phone phone2 = new Phone();
+
+        new Thread(() -> {
+            try {
+                phone.sendEmail();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }, "A").start();
+
+        Thread.sleep(100);
+
+        new Thread(() -> {
+            try {
+                //phone.sendSMS();
+                //phone.sayHello();
+                phone2.sendSMS();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }, "B").start();
+
+    }
+
+
+    static class Phone {
+        public synchronized void sendEmail() throws Exception {
+            TimeUnit.SECONDS.sleep(4);
+            System.out.println("*****sendEmail");
+        }
+
+        public synchronized void sendSMS() throws Exception {
+            System.out.println("*****sendSMS");
+        }
+
+        public void sayHello() throws Exception {
+            System.out.println("*****sayHello");
+        }
+    }
+}
+
+
+
+
+
+
+
---
+++ src/main/java/com/yfy/multithreading/demo/NotSafeDemo03.java
@@ -0,0 +1,84 @@
+package com.yfy.multithreading.demo;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+/**
+ * 1 故障现象
+ * java.util.ConcurrentModificationException
+ * <p>
+ * 2 导致原因
+ * <p>
+ * 3 解决方法
+ * 3.1 new Vector<>()
+ * 3.2 Collections.synchronizedList(new ArrayList<>());
+ * 3.3 new CopyOnWriteArrayList()
+ * <p>
+ * <p>
+ * 4 优化建议(同样的错误不犯第2次)
+ */
+public class NotSafeDemo03 {
+    public static void main(String[] args) {
+        Map<String, String> map = new ConcurrentHashMap<>();
+        for (int i = 1; i <= 30; i++) {
+            new Thread(() -> {
+                map.put(Thread.currentThread().getName(), UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(map);
+            }, String.valueOf(i)).start();
+        }
+    }
+
+    public static void setNotSafe() {
+        Set<String> set = new CopyOnWriteArraySet<>();
+        for (int i = 1; i <= 30; i++) {
+            new Thread(() -> {
+                set.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(set);
+            }, String.valueOf(i)).start();
+        }
+    }
+
+    public static void listNotSafe() {
+        List<String> list = new CopyOnWriteArrayList();//Collections.synchronizedList(new ArrayList<>());//new Vector<>();//new ArrayList<>();
+
+        for (int i = 1; i <= 30; i++) {
+            new Thread(() -> {
+                list.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(list);
+            }, String.valueOf(i)).start();
+        }
+    }
+}
+
+
+/**
+ * 笔记
+ * 写时复制
+ * CopyOnWrite容器即写时复制的容器。往一个容器添加元素的时候，不直接往当前容器Object[]添加，而是先将当前容器Object[]进行Copy，
+ * 复制出一个新的容器Object[] newElements，然后新的容器Object[] newElements里添加元素，添加完元素之后，
+ * 再将原容器的引用指向新的容器 setArray(newElements);。这样做的好处是可以对CopyOnWrite容器进行并发的读，
+ * 而不需要加锁，因为当前容器不会添加任何元素。所以CopyOnWrite容器也是一种读写分离的思想，读和写不同的容器
+ * public boolean add(E e)
+ * {
+ * final ReentrantLock lock = this.lock;
+ * lock.lock();
+ * <p>
+ * try
+ * {
+ * Object[] elements = getArray();
+ * int len = elements.length;
+ * Object[] newElements = Arrays.copyOf(elements, len + 1);
+ * newElements[len] = e;
+ * setArray(newElements);
+ * return true;
+ * }
+ * finally {
+ * lock.unlock();
+ * }
+ * }
+ */
