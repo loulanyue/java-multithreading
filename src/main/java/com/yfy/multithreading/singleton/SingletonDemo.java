1ec03d71b5318b027f69a89636618429cdb0c3c4
loulanyue <260355617@qq.com>
2020-06-28 22:54:42 +0800

    引入线程池

--- src/main/java/com/yfy/multithreading/singleton/SingletonDemo.java
+++ src/main/java/com/yfy/multithreading/singleton/SingletonDemo.java
@@ -1,11 +1,24 @@
 package com.yfy.multithreading.singleton;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author youfyu
  * @date 2020/06/28
- *  单例模式线程不安全情况下，加入双端检锁机制，通过volatile禁止指令重排
+ * 单例模式线程不安全情况下，加入双端检锁机制，通过volatile禁止指令重排
  */
 public class SingletonDemo {
+    //引入guava依赖包，创建线程池
+    private static ThreadFactory namedThreadFactory = new ThreadFactoryBuilder().setNameFormat("thread-call-runner-%d").build();
+
+    private static ExecutorService taskExe = new ThreadPoolExecutor(10, 20, 200L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(), namedThreadFactory);
+
 
     private volatile static SingletonDemo instance = null;
 
@@ -26,20 +39,12 @@ public class SingletonDemo {
     }
 
     public static void main(String[] args) {
-        //单线程（main线程的操作动作）
-//        System.out.println(SingletonDemo.getInstance() == SingletonDemo.getInstance());
-//        System.out.println(SingletonDemo.getInstance() == SingletonDemo.getInstance());
-//        System.out.println(SingletonDemo.getInstance() == SingletonDemo.getInstance());
-//
-//        System.out.println("------");
-//        System.out.println("------");
-//        System.out.println("------");
 
-        //并发多线程后，不加synchronized结果发生改变，没有唯一结果
+        //并发多线程时，不加synchronized结果发生改变，没有唯一结果
         for (int i = 1; i <= 10; i++) {
-            new Thread(() -> {
-                SingletonDemo.getInstance();
-            }, String.valueOf(i)).start();
+
+            taskExe.execute(() -> SingletonDemo.getInstance());
+
         }
     }
 }
